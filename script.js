93738b75517e7cee2a50efe0f86827b0d251c1ac
fifidonnelly <126438786+fifidonnelly@users.noreply.github.com>
2023-04-11 16:38:58 +0800

    amended format

--- script.js
+++ script.js
@@ -8,9 +8,7 @@ function say(myMessage) {
   
   } 
  
-  /* This is a simplified version of the game.
-        The Computer ALWAYS picks ROCK.  
-        Does the user's choose win,lose, or tie vs ROCK? */
+  /* This is a simplified version of the game. The Computer ALWAYS picks ROCK. Does the user's choose win,lose, or tie vs ROCK? */
         function play(user) {
             let result = "";
             if (user === 'rock') {
